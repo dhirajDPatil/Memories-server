88ece00aa3e51aadf10cb076125a663d797ec672
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-08-23 21:15:28 +0530

    first commit

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import cors from "cors";
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
-// dhirajpatil
-// dhirajpatil123
 const app = express();
 
 dotenv.config();
@@ -21,10 +19,9 @@ app.get('/', (req,res)=> {
     res.send('Hello to memories APIS')
 })
 
-// const CONNECTION_URL = 'mongodb+srv://dhirajpatil:dhirajpatil123@cluster0.2v4mycb.mongodb.net/memory?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((err)=> console.log(err.massage));
-// mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);
