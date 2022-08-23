a10ab5faaa8de5fe0029102343a0e24b5ae247cd
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-08-24 00:17:48 +0530

    make it better4

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+import dotenv from 'dotenv'; 
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
-const app = express();
-
 dotenv.config();
+
+const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -15,15 +15,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-app.get('/', (req,res)=> {
-    res.send('Hello to memories APIS')
+app.get('/', (req,res) => {
+    res.send('Hello to memories APIS');
 })
 
 const host = '0.0.0.0';
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> app.listen(PORT, host, ()=> console.log(`Server running on port: ${PORT}`)))
+    .then(()=> app.listen(port, host, ()=> console.log(`Server running on port: ${port}`)))
     .catch((err)=> console.log(err.massage));
 // mongoose.set('useFindAndModify', false);
