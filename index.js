d82dc04b1535ea511ee4a8b0b42817ea86bf9041
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-09-25 23:49:22 +0530

    Git push

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import postRoutes from './routes/posts.js';
 
 dotenv.config();
-
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
@@ -25,4 +24,4 @@ const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(port, host, ()=> console.log(`Server running on port: ${port}`)))
-    .catch((err)=> console.log(err.massage));
+    .catch((err)=> console.log(err.message));
